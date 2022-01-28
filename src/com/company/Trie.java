b2d69dae5cdd0c97a704f7d5a6c5968c64cc8ace
Arah Gholamdokht <gholamdokht.arash@gmail.com>
2022-01-28 12:46:48 +0330

    add Auto Completion to Trie class

--- src/com/company/Trie.java
+++ src/com/company/Trie.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Trie {
@@ -28,7 +29,7 @@ public class Trie {
         }
     }
 
-    private TrieNode root = new TrieNode(' ');
+    private final TrieNode root = new TrieNode(' ');
 
     private void insert(String str) {
         TrieNode current = root;
@@ -41,5 +42,34 @@ public class Trie {
             current = current.children[index];
         }
         current.isFullWord = true; // Confirm the placement of a word for further usages
+    }
+
+    public String[] findWords(String prefix){
+        TrieNode node = lastNode(prefix);
+        LinkedList<String> words = new LinkedList<>();
+        if (node != null)
+            suggestionWords(node, prefix, words);
+
+        return words.toArray(new String[0]);
+    }
+
+    private void suggestionWords(TrieNode root, String prefix, LinkedList<String> words){
+        if (root.isFullWord)
+            words.add(prefix);
+        for (int i = 0; i < 26; i++){
+            if (root.children[i] != null)
+                suggestionWords(root.children[i], prefix + root.children[i].value, words);
+        }
+    }
+    private TrieNode lastNode(String prefix){
+        TrieNode current = root;
+        for (int i = 0; i < prefix.length(); i++){
+            int index = prefix.charAt(i) - 97;
+            TrieNode child = root.children[index];
+            if (child == null)
+                return null;
+            current = child;
+        }
+        return current;
     }
 }
